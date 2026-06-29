deebc0490e671325b877a46c0cb76da83308acc2
Evelien <edewals2009@gmail.com>
2026-06-29 12:34:56 +0200

    changed from raw image to image

--- Assets/Scripts/UI/DialogueButton.cs
+++ Assets/Scripts/UI/DialogueButton.cs
@@ -7,15 +7,15 @@ public class DialogueButton : MonoBehaviour
     [Tooltip("put in the dialogues you want to play, 0 is the first to play")]
     [SerializeField] private List<DialogueData> dialoguesToPlay;// = new();
     [Header("sprites")]
-    [SerializeField] private RawImage backgroundSprite;
-    [SerializeField] private List<Texture> spritesToPlay;// = new();
+    [SerializeField] private UnityEngine.UI.Image backgroundSprite;
+    [SerializeField] private List<Sprite> spritesToPlay;// = new();
     [SerializeField] private GameObject sceneswapButton;
     int currentDialogue = 0;
 
     public void playNextDialogue()
     {
         DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
-        backgroundSprite.texture = spritesToPlay[currentDialogue];
+        backgroundSprite.sprite = spritesToPlay[currentDialogue];
         if (currentDialogue < dialoguesToPlay.Count - 1)
         {
             currentDialogue++;
