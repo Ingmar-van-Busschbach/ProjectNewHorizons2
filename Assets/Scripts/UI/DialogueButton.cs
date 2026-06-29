52f623ab0672ab3ea30f11a00222b69c9deb3a26
Evelien <edewals2009@gmail.com>
2026-06-29 12:24:02 +0200

    changing background now properly works

--- Assets/Scripts/UI/DialogueButton.cs
+++ Assets/Scripts/UI/DialogueButton.cs
@@ -1,30 +1,27 @@
-using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueButton : MonoBehaviour
 {
     [Tooltip("put in the dialogues you want to play, 0 is the first to play")]
     [SerializeField] private List<DialogueData> dialoguesToPlay;// = new();
     [Header("sprites")]
-    [SerializeField] private Sprite backgroundSprite;
-    [SerializeField] private List<Sprite> spritesToPlay;// = new();
+    [SerializeField] private RawImage backgroundSprite;
+    [SerializeField] private List<Texture> spritesToPlay;// = new();
     [SerializeField] private GameObject sceneswapButton;
     int currentDialogue = 0;
+
     public void playNextDialogue()
     {
+        DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
+        backgroundSprite.texture = spritesToPlay[currentDialogue];
         if (currentDialogue < dialoguesToPlay.Count - 1)
         {
-            DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
-            Debug.Log(dialoguesToPlay[currentDialogue]);
-            backgroundSprite = spritesToPlay[currentDialogue];
             currentDialogue++;
         }
         else
-        {
-            DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
+        { 
             this.gameObject.SetActive(false);
             sceneswapButton.SetActive(true); 
         }
