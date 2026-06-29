7c03c5b8930ea96e8bd731dc580fe868b0736cff
Evelien <edewals2009@gmail.com>
2026-06-29 12:06:36 +0200

    fixed small issues in scene and allows player to swap scenes to play after the dialogue

--- Assets/Scripts/UI/DialogueButton.cs
+++ Assets/Scripts/UI/DialogueButton.cs
@@ -1,20 +1,33 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DialogueButton : MonoBehaviour
 {
     [Tooltip("put in the dialogues you want to play, 0 is the first to play")]
-    [SerializeField] private List<DialogueData> dialoguesToPlay = new();
+    [SerializeField] private List<DialogueData> dialoguesToPlay;// = new();
     [Header("sprites")]
     [SerializeField] private Sprite backgroundSprite;
-    [SerializeField] private List<Sprite> spritesToPlay = new();
+    [SerializeField] private List<Sprite> spritesToPlay;// = new();
+    [SerializeField] private GameObject sceneswapButton;
     int currentDialogue = 0;
     public void playNextDialogue()
     {
-        DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
-        backgroundSprite = spritesToPlay[currentDialogue];
-        currentDialogue++;
-        
+        if (currentDialogue < dialoguesToPlay.Count - 1)
+        {
+            DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
+            Debug.Log(dialoguesToPlay[currentDialogue]);
+            backgroundSprite = spritesToPlay[currentDialogue];
+            currentDialogue++;
+        }
+        else
+        {
+            DialogueWriter.Instance.InitializeDialogue(dialoguesToPlay[currentDialogue]);
+            this.gameObject.SetActive(false);
+            sceneswapButton.SetActive(true); 
+        }
+
     }
 }
