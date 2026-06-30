d4a5b6da181d79dd7996b400142df14226e3af78
Ingmar van Busschbach <ingmarmatthis@gmail.com>
2026-06-30 13:13:07 +0200

    Added auto-hide for the quit button on WebGL builds.

--- Assets/Scripts/UI/QuitGame.cs
+++ Assets/Scripts/UI/QuitGame.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
+#if UNITY_WEBGL
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+#endif
     public void HandleQuit()
     {
         Application.Quit();
